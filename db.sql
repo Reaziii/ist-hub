19955949d13489a5618121355c85f16403617ea5
JAMAN KHAN <jamankhanjk5@gmail.com>
2024-03-20 02:44:44 +0600

    Initial create queries added

--- db.sql
+++ db.sql
@@ -11,15 +11,58 @@ CREATE TABLE IF NOT EXISTS user(
     password varchar(100) NOT NULL,
     photo varchar(100) NOT NULL,
     verified boolean,
-    primary key(userid)
+    PRIMARY KEY(userid)
 
 );
-CREATE TABLE IF NOT EXISTS edication(
-    userid int,
+
+CREATE TABLE IF NOT EXISTS education(
+    userid int NOT NULL,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
     grade FLOAT(2,2) NOT NULL,
     degree varchar(20) NOT NULL,
     school varchar(100) NOT NULL,
+    FOREIGN KEY (userid) REFERENCES user(userid)
+);
+
+CREATE TABLE IF NOT EXISTS projects(
+    projectid int NOT NULL AUTO_INCREMENT,
+    userid int NOT NULL,
+    photo varchar(100) NOT NULL,
+    title varchar(100) NOT NULL,
+    description varchar(500) NOT NULL,
+    PRIMARY KEY(projectid),
+    FOREIGN KEY (userid) REFERENCES user(userid)
+);
+
+CREATE TABLE IF NOT EXISTS project_tech(
+    projectid int NOT NULL,
+    techname varchar(50) NOT NULL,
+    FOREIGN KEY (projectid) REFERENCES projects(projectid)
+);
+
+CREATE TABLE IF NOT EXISTS experiance(
+    userid int NOT NULL,
+    experianceid int NOT NULL AUTO_INCREMENT,
+    title varchar(100) NOT NULL,
+    employee_type varchar(100) NOT NULL,
+    position varchar(100) NOT NULL,
+    company_name varchar(200) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    PRIMARY KEY(experianceid),
+    FOREIGN KEY (userid) REFERENCES user(userid)
+);
+
+CREATE TABLE IF NOT EXISTS experiance_tech(
+    experianceid int NOT NULL,
+    techname varchar(50) NOT NULL,
+    FOREIGN KEY (experianceid) REFERENCES experiance(experianceid)
+);
+
+CREATE TABLE IF NOT EXISTS verify(
+    verifiedby int NOT NULL,
+    userid int NOT NULL,
+    FOREIGN KEY (verifiedby) REFERENCES user(userid),
     FOREIGN KEY (userid) REFERENCES user(userid)
 );
