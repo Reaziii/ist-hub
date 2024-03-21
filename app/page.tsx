a33a63caae6eb981fe9edbb7cda6c3b0751c361f
Reaz Ahammed <baphonreaz@gmail.com>
2024-03-21 12:44:23 +0600

    small fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 export default function Home() {
 
   return (
-    <main className="h-[100vh] w-full bg-main">
+    <main className="h-[100vh] w-ful">
       <Header />
 
     </main>
